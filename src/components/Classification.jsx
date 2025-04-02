3bab052d52a25872500cea0cec059d95f597eab8
VITOR PAIVA NUNES DE PAULA <vitor.paula19@etec.sp.gov.br>
2025-04-02 11:32:35 -0300

    Quick fixes

--- src/components/Classification.jsx
+++ src/components/Classification.jsx
@@ -7,19 +7,19 @@ const Classification = ({imc}) => {
     const [classification, setClassification] = useState();
     switch (imc){
         case imc < 18.5:
-            classification = setClassification("Abaixo do peso");
+            setClassification("Abaixo do peso");
         break;
         case imc >= 18.5 && imc <= 24.9:
-            classification = setClassification("Sobrepeso");
+            setClassification("Sobrepeso");
         break;
         case imc >= 25 && imc <= 29.9:
-            classification = setClassification("Obesidade grau 1");
+            setClassification("Obesidade grau 1");
         break;
         case imc >=30 && imc <= 39.9:
-            classification = setClassification("Obesidade grau 2");
+            setClassification("Obesidade grau 2");
         break;
         case imc >= 40:
-            classification = setClassification("Obesidade grau 3 (Obesidade mórbia)");
+            setClassification("Obesidade grau 3 (Obesidade mórbia)");
         break;
 
         return(
